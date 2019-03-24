8ef2fa30c4a2abf0ccbb1fe3ad079991479295e9
andes <wenta2006@gmail.com>
2019-03-24 23:28:57 +0800

    add godoc.sh

---
+++ godoc.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#/usr/local/go/bin/godoc -http=:6060 >/dev/null 2>&1 &
+/project/go/bin/godoc -http=:6060 >/dev/null 2>&1 &
+#/project/go/src/code.google.com/p/go-tour/gotour/gotour >/dev/null 2>&1 &
